745a26a4ec231aa8a6c9f3b5efcfd5ce890bc7d5
Trevor Hanus <trevorhanus@gmail.com>
2023-10-04 13:22:40 -0400

    moved gravity legal hook

--- src/components/HostedFieldInput.tsx
+++ src/components/HostedFieldInput.tsx
@@ -8,7 +8,7 @@ import {
   Spinner,
 } from '@chakra-ui/react';
 import { FC } from 'react';
-import { FieldState } from './hooks/GravityLegal';
+import { FieldState } from '../gravity-legal-hook/GravityLegal';
 
 export interface HostedFieldInputProps {
   id: string;
